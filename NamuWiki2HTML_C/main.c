d44f38d1272483595f3d9725d610d2025426ce43
Qewin <inoutbeing@gmail.com>
2016-07-23 12:38:20 +0900

    I/O 개선, 안정
    다음 목표 : 멀티쓰레드 파서 실행

--- NamuWiki2HTML_C/main.c
+++ NamuWiki2HTML_C/main.c
@@ -1,46 +1,49 @@
 #include <stdio.h>
 #include "stdnamu.h"
+//#include <pthread.h>
 
 #define CORE 3 //코어 수 - 1 
 #define Csize  CORE*50*1000*1000
-//#define forcores for(short i = 0; i<CORE; i++) // 코어 수에 맞춰 호출 
+
+int ReadJSON(FILE *input){ //[1~] : 각 문서의 포인터 반환. [0] : 원래 문서의 포인터가 들어 있음.(2번째 로딩부터는 사용.)
+	char *cache;
+	int *document;
+	if( (cache = (char *)malloc(Csize)) == NULL) return NULL;
+	if( (document = (int *)malloc(sizeof(int)*500000)) == NULL) return NULL;
+	int iv, a;
+	for (a = iv = 0; (Csize - a) >10000; iv++) {
+		document[iv] = &cache[a]; //{"n 에서 n이 반환됨. 
+		while( (cache[a++] = getc(input)) != '\"' ) while( (cache[a++] = getc(input)) != '{' ); // EOF 도. feof();
+	} // 마지막은 기록 안됨. 
+	for(a= 1; a<CORE; a++) document[iv+a] = '\0'; 
+	printf("%d,%d",iv,a); //for(a = 0; a < iv;a++)printf("%d,",document[a]);
+	return document;
+}
+
 
 int JsonIO(){
 	//구조를 하나의 큰 malloc에서 나머지로 편성하는 방법 사용하기 
 	FILE *input;
 	if((input = fopen("namu.json","r")) == NULL) return 1;
-	char *cache;
-	if((cache = (char *)malloc(Csize)) == NULL) return 2;
-	//read near 50MB * (core - 1) (maybe 4000 documents?)
-	int document[500000];
-	int iv, a;
-	a = 0;
+	int *dptr;
+	if( (dptr = ReadJSON(input) ) == NULL) return 2;
 	
-	document[0] = &cache[0];
-	for (iv = 1; (Csize - (&cache[a] - cache)) >10000; iv++) {
-		while( (cache[a] = getc(input)) != '}' ) a++; // EOF 도. feof();
-		a++; 
-		document[iv] = &cache[a];
-	}
-	document[iv+1] = '\0';
-	//printf("%s\n",cache);
-	//printf("%d",(&cache[5]-cache));
-	printf("%d",iv);
-	scanf("%d",&a);
+	
+	free (dptr[0]);
+	
 	//send to pointer
 	//while all the pointer is not null : 
 	// until 40% of memory left
 	//  read near 50MB
 	//  find title, namespace, document
 	//start new parse thread
-	free (cache);
 	return 0;
 }
 
-int ParseManager(){
-	//
+void *worker(void *arg){
 	
-	return 0;
+	
+//	pthread_exit(NULL);
 }
 
 int main(){
